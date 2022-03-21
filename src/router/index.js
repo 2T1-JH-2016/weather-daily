749c6a16a6a8995ff3f62b9aaf8bf3fabbca856c
2T1-JH-2016 <jh20160528@gmail.com>
2022-03-21 23:01:01 +0900

    Add calendar

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WriteView.vue')
-  }
+  },
+  {
+    path: '/diary',
+    name: 'diary',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DiaryView.vue')
+  },
 ]
 
 const router = new VueRouter({
--- src/views/DiaryView.vue
+++ src/views/DiaryView.vue
@@ -2,105 +2,189 @@
     <v-container>
         <v-row>
             <v-col class="userInfo" :style="$vuetify.breakpoint.width > 600 ? 'max-width:600px' :''">
-                <v-card class="pl-10 pr-10">
-                    <v-card-title>                           
-                        <v-row class="d-flex">
-                            <v-col cols="6" align="left" class="pb-0 pl-0 text-overline">
-                                <DatePicker v-model="diary.date" @updateDate="updateDate"></DatePicker>
-                                <font-awesome-icon class="ml-1" :icon="getWeatherIcon(diary.weather)" />
-                            </v-col> 
-                            <v-col class="d-flex flex-row-reverse" align="left" cols="6">
-                                <v-tooltip bottom>
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <div class="mt-1 text-caption text-truncate text-right" 
-                                            style="max-width: 100%;"
-                                            v-bind="attrs"
-                                            v-on="on">
-                                            <font-awesome-icon icon="fa-solid fa-location-dot" />
-                                            <span class="ml-2">{{diary.location}}</span>
-                                        </div>
-                                    </template>
-                                    <span>{{diary.location}}</span>
-                                </v-tooltip>
-                            </v-col>
-                        </v-row>
-                    </v-card-title>
-
-                    <v-textarea
-                        label="짧은 일기"
-                        auto-grow
-                        outlined
-                        rows="4"
-                        row-height="30"
-                        shaped
-                        maxlength="200"
-                        v-model="diary.content"
-                        color="orange orange-darken-4"
-                        :messages="diary.content.length+' / 200'"
-                    ></v-textarea>
-                    <v-row>
-                        <v-col align="right">
-                            <v-btn
-                                color="orange orange-darken-4"
-                                depressed
-                                dark
-                                @click="saveDiary"
-                            ><span class="font-weight-bold">OK</span></v-btn>
-                        </v-col>
-                    </v-row>
-                </v-card>
+                <v-sheet height="64">
+        <v-toolbar
+          flat
+        >
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="prev"
+          >
+            <v-icon small>
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+          <v-btn
+            outlined
+            color="grey darken-2"
+            @click="setToday"
+          >
+            Today
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="next"
+          >
+            <v-icon small>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="550">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          :events="events"
+          :event-color="getEventColor"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <v-btn icon>
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent.details"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
             </v-col>
         </v-row>
     </v-container>
 </template>
   
 <script>
-    import DatePicker from '@/components/DatePicker'
-    export default {
-        name: 'WriteView',
-        components : { DatePicker },
-        data: () => ({
-            menu: false,
-            diary : {
-                date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-                weather : 0,
-                location : 'asdgasdgasdgasdg',
-                author : '이태원',
-                title : 'asdgasdgasdg',
-                content : 'asdgasdgsagasdg',
-            }
-        }),
-        methods : {
-            updateDate:function(date){
-                this.diary.date = date
-            },
-            getWeatherIcon:function(value){
-                console.log(value)
-            
-                switch(value){
-                    case 0 : 
-                    return "fa-solid fa-sun" //sun
-                    case 1 :
-                    return "fa-solid fa-snowflake" //snow
-                    case 2 : 
-                    return "fa-solid fa-cloud" //cloud
-                    case 3 : 
-                    return "fa-solid fa-sun-cloud" //sun-cloud
-                    case 4 : 
-                    return "fa-solid fa-umbrella-simple" //rain
-                    case 5 : 
-                    return "fa-solid fa-wind" //wind
-                    default :
-                    return "fa-solid fa-sun" //sun
-                }
-            },
-            saveDiary:function(){
-                alert("ddd");
-            }
-        },
-        computed:{
+  export default {
+    data: () => ({
+      focus: '',
+      type: 'month',
+      typeToLabel: {
+        month: 'Month',
+        week: 'Week',
+        day: 'Day',
+        '4day': '4 Days',
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [],
+      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+    }),
+    mounted () {
+      this.$refs.calendar.checkChange()
+    },
+    methods: {
+      viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = ''
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
         }
-    }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          requestAnimationFrame(() => requestAnimationFrame(() => open()))
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+      updateRange ({ start, end }) {
+        const events = []
+
+        const min = new Date(`${start.date}T00:00:00`)
+        const max = new Date(`${end.date}T23:59:59`)
+        const days = (max.getTime() - min.getTime()) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+
+        for (let i = 0; i < eventCount; i++) {
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
