424ff71344e3bb8a23acfdaec6fcc6f833949414
2T1-JH-2016 <jh20160528@gmail.com>
2022-03-28 21:11:44 +0900

    로그인 관련 store 생성

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userInfo : {
+      id : '',
+      uid : '',
+      path : '',
+    }
   },
   getters: {
+    getUserInfo(state){
+      return state.userInfo
+    }
   },
   mutations: {
+    setUserInfo(state,payload){
+      state.userInfo.id = payload.id
+      state.userInfo.path = payload.path
+      state.userInfo.uid = payload.uid //todos : 다르게 가져올 수 잇는지 확인 필요
+    }
   },
   actions: {
+    setUserInfo : ({commit},payload) =>{
+      commit('setUserInfo',payload)
+    }
   },
   modules: {
   }
